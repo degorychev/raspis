7db98e4f215a958c4d4c3570eeab948b545873e7
Денис Егорычев <den.egorychev@gmail.com>
2018-03-10 00:05:49 +0400

    метрика

--- components/navbar.php
+++ components/navbar.php
@@ -16,5 +16,8 @@
             <li><a href="?page=all-par">На всю неделю</a></li>
             <li><a href="old">Старая версия</a></li>
         </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li> <a style="padding-top: 10px; padding-bottom: 10px" href="https://metrika.yandex.ru/stat/?id=47804353&amp;from=informer" target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/47804353/3_1_FFFFFFFF_EFEFEFFF_0_pageviews" style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="47804353" data-lang="ru" /></a> <!-- /Yandex.Metrika informer --> <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter47804353 = new Ya.Metrika({ id:47804353, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/47804353" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter --></li>
+        </ul>
     </div>					
 </div>
